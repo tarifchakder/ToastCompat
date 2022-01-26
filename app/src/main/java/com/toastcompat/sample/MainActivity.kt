7505c31826e77c802e8cafec0b462cdcda4f2710
MR Tech <tarifchakder@outlook.com>
2022-01-26 12:18:15 +0530

    Fix

--- app/src/main/java/com/toastcompat/sample/MainActivity.kt
+++ app/src/main/java/com/toastcompat/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.toastcompat.sample.databinding.ActivityMainBinding
+import io.tarif.toastcompat.ktx.ToastCompat
 
 class MainActivity : AppCompatActivity() {
 
